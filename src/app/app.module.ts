aad193d8bb9acf5588f5db0061fad35edcf16ec1
roushanverma123 <roushanverma123@gmail.com>
2019-11-29 04:17:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Template1Component } from './template1/template1.component';
+import { MaincompComponent } from './maincomp/maincomp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Template1Component,
+    MaincompComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/maincomp/maincomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maincomp',
+  templateUrl: './maincomp.component.html',
+  styleUrls: ['./maincomp.component.css']
+})
+export class MaincompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
